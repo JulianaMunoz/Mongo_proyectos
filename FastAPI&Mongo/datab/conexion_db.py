eb018f6edd60642f533cb202cb4bb666b5352752
Juan Garcia <spdjuanse@gmail.com>
2025-04-08 18:31:26 -0500

    Apis

--- FastAPI&Mongo/datab/conexion_db.py
+++ FastAPI&Mongo/datab/conexion_db.py
@@ -1,3 +1,4 @@
+
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
@@ -9,15 +10,16 @@ client = None
 
 async def connect_to_mongo():
     global client
-    client = AsyncIOMotorClient(MONGO_URI)
+    client = AsyncIOMotorClient(MONGO_URI, tls=True, tlsAllowInvalidCertificates=False)
     print("Conectado a MongoDB")
 
 async def close_mongo_connection():
     global client
-    client.close()
-    print("Desconectado de MongoDB")
+    if client:
+        client.close()
+        print("Desconectado de MongoDB")
 
 def get_database():
     if client is None:
         raise HTTPException(status_code=500, detail="No hay conexión a la base de datos")
-    return client.turismo  # Nombre de la base de datos
+    return client.items
--- FastAPI&Mongo/main.py
+++ FastAPI&Mongo/main.py
@@ -1,27 +1,25 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 from routes.crud_routes import router as crud_router
 from datab.conexion_db import connect_to_mongo, close_mongo_connection
-from dotenv import load_dotenv
-from fastapi.middleware.cors import CORSMiddleware
 
-
-load_dotenv()  # Carga las variables de entorno
-
-origins = [
-    "http://localhost:5500",
-]
+load_dotenv() 
 
 app = FastAPI()
 
 
+origins = ["http://localhost:5500"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,      # Permite estos orígenes
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 @app.on_event("startup")
 async def startup_db_client():
     await connect_to_mongo()
@@ -30,5 +28,4 @@ async def startup_db_client():
 async def shutdown_db_client():
     await close_mongo_connection()
 
-# Incluye las rutas con sus respectivos prefijos y tags
-app.include_router(crud_router, prefix="/crud", tags=["items"])
+app.include_router(crud_router, tags=["items"])
