4235e73ad7f4d1cfa8b9d1514d60d186a637ab55
JulianaMunoz <jumemu2002@hotmail.com>
2025-04-08 21:12:28 -0500

    adding another category

--- FastAPI&Mongo/models/pet_model.py
+++ FastAPI&Mongo/models/pet_model.py
@@ -3,4 +3,4 @@ from typing import Literal
 
 class PetRequest(BaseModel):
     tipo: Literal["perro", "gato", "conejo"]
-    personalidad: Literal["jugueton", "tranquilo", "valiente"]
+    personalidad: Literal["jugueton", "tranquilo", "valiente", "Tragon"]
--- FastAPI&Mongo/routes/petNames_routes.py
+++ FastAPI&Mongo/routes/petNames_routes.py
@@ -8,17 +8,20 @@ nombres_por_tipo = {
     "perro": {
         "jugueton": ["Pelusa", "Saltín", "Toby", "Max"],
         "tranquilo": ["Don Siesta", "Coco", "Luna", "Rocky"],
-        "valiente": ["Thor", "Rex", "Bruno", "Kira"]
+        "valiente": ["Thor", "Rex", "Bruno", "Kira"],
+        "Tragon": ["Pancho", "Chester", "Odin", "Bubba"]
     },
     "gato": {
         "jugueton": ["Mishi", "Zoom", "Pelusín", "Nube"],
         "tranquilo": ["Don Ronroneo", "Chispa", "Sombra", "Mora"],
-        "valiente": ["Garfield", "Zelda", "Pantera", "Tigre"]
+        "valiente": ["Garfield", "Zelda", "Pantera", "Tigre"],
+        "Tragon": ["Gordito", "Garfield", "Munchkin", "Panzón"]
     },
     "conejo": {
         "jugueton": ["Brinco", "Pompon", "Pelusa", "Saltarín"],
         "tranquilo": ["Nube", "Dumbo", "Luna", "Daisy"],
-        "valiente": ["Trueno", "Furia", "Blitz", "Sonic"]
+        "valiente": ["Trueno", "Furia", "Blitz", "Sonic"],
+        "Tragon": ["Blaze", "Chonky", "Bunny", "Fluffy"]
     }
 }
 
